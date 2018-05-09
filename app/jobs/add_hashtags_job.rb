3716e9761f867b920973a38284972682b59b7cd8
Flavio Carvalho <flavio@globalnetsis.com.br>
2018-05-09 10:00:55 -0300

    Implementando o Trending Topics – Criando os Jobs

---
+++ app/jobs/add_hashtags_job.rb
@@ -0,0 +1,7 @@
+class AddHashtagsJob < ApplicationJob
+  queue_as :default
+
+  def perform(*args)
+    # Do something later
+  end
+end
---
+++ app/jobs/update_trendings_job.rb
@@ -0,0 +1,7 @@
+class UpdateTrendingsJob < ApplicationJob
+  queue_as :default
+
+  def perform(*args)
+    # Do something later
+  end
+end
